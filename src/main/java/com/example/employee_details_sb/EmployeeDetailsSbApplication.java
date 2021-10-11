0c22a74ab11d6961df7255498852b481e636d3a2
PraneshBalasubramaniam <praneshbk240599@gmail.com>
2021-10-11 13:57:02 +0530

    Created spring boot Application and given properties

---
+++ src/main/java/com/example/employee_details_sb/EmployeeDetailsSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_details_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDetailsSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDetailsSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employee_details_sb/EmployeeDetailsSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_details_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDetailsSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
